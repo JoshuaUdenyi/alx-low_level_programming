701519abed34ae37bafe29b930399b83b22fb273
JoshuaUdenyi <udenyijoshua95@gmail.com>
2022-07-12 10:54:40 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * Write a function that returns the length of a string
+ * @a : where the value is stored
+ */
+
+int _strlen(char *s)
+{
+	char a;
+
+	_putchar("Length of String a = %d\n", strlen(a));
+
+	return 0;
+}
